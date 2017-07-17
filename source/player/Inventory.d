19f05e76e581a42cdbc775b61b70356ede91b063
igor-pea <epgorokhovsky01@bvsd.org>
2017-07-17 15:48:00 -0600

    Revert "Merge branch 'master' into Plants-Dev"
    This reverts commit 2843fe39a33f2f8f65777fa8336f762e8192bff0, reversing
    changes made to 9f45605dd4aec38390dd254dbab31a2dd47f74dd.

--- source/player/Inventory.d
+++ source/player/Inventory.d
@@ -1,14 +1,15 @@
 import Player;
-import TilePiece;
+import Item;
 import core.exception;
 
 /**
  * A class for an object that holds other objects
+ * TODO add iterator for a foreach loop
  */
 class Inventory{
 
     public Player[] accessibleTo;   ///A list of players who can access the inventory
-    private TilePiece[] items;      ///All the items inside of the inventory
+    public Item[] items;       ///All the items inside of the inventory
     public int maxSize;             ///The maximum number of elements in the inventory; if is negative, the array can have infinite elements
     public int[] coords;            ///The location of the tile or the player or inventorycontainer with this inventory
 
@@ -17,68 +18,89 @@ class Inventory{
      * Usually for players, but may also be needed for improvements/plants
      * Takes in a size, but if the inventory size is negative, the inventory has infinite size
      */
-    this(int maxSize){
-        items = (maxSize < 0)? null : new TilePiece[maxSize];
+    this(int maxSize = -1){
+        this.maxSize = maxSize;
+        items = (maxSize < 0)? null : new Item[maxSize];
     }
 
     /**
-     * Attempts to add an item to the inventory, and returns whether the operation was successful or not
+     * Adds the given tile piece to the inventory
+     * Returns whether the tile piece was successfully added to inventory
      * Params:
-     *      toAdd = the item to add to the inventory
+     *      itemToAdd = the item to add to the inventory
      */
-    public bool addItem(TilePiece toAdd){
-        try{
-            this.items ~= toAdd;
-            return true;
-        }catch(RangeError e){
-            return false;
-        }
-    }
-
-    /**
-     * Gets the number of tile pieces in the inventory
-     */
-    public int getSize(){
+    public bool add(Item itemToAdd){
         if(this.maxSize < 0){
-            return this.items.length;
+            this.items ~= itemToAdd;
+            return true;
         }
-        int size = 0;
-        foreach(TilePiece item ; this.items){
-            if(item !is null){
-                size++;
-            }else{
-                return size;
+        for(int i = 0; i < this.items.length; i++){
+            if(this.items[i] is null){
+                this.items[i] = itemToAdd;
+                return true;
             }
         }
-        return 0;
+        return false;
     }
 
-    public int getCollectiveMovementCost(){
-        return 0;
+    /**
+     * Removes the given tile piece to the inventory and sets its location in the items array to null
+     * Returns the tile piece given it was successfully removed, otherwise returns null
+     * Params:
+     *      itemToRemove = the item to remove from the inventory
+     */
+    public Item remove(Item itemToRemove){
+        for(int i = 0; i < this.items.length; i++){
+            if(this.items[i] == itemToRemove){
+                this.items[i] = null;
+                return itemToRemove;
+            }
+        }
+        return null;
     }
 
+    /**
+     * Gets the movement cost of all of the items in the inventory
+     */
+    public double getCollectiveMovementCost(){
+        double moveCost = 0;
+        foreach(Item item ; this.items){
+           if(item !is null && item.isPlaced){
+                moveCost += item.getMovementCost();
+           }
+        }
+        return moveCost;
+    }
+
+    /**
+     * Gets the collective value of all of the items in the inventory
+     * TODO
+     */
     public int getCollectiveValue(){
         return 0;
     }
 
-    public TilePiece get(int index){
-        return null;
-    }
-
-    public TilePiece remove(int index){
-        return null;
-    }
-
-    public TilePiece remove(TilePiece toRemove){
-        return null;
-    }
-
-    public bool doesExist(TilePiece item){
-        return false;
+    /**
+     * Returns a copy of this inventory
+     * Changes in this inventory are not reflected in the copy
+     * Changes in players are reflected in the copy, however
+     */
+    public Inventory clone(){
+        Inventory copy = new Inventory(this.maxSize);
+        for(int i = 0; i < this.items.length; i++){
+            copy.items[i] = this.items[i].clone();
+        }
+        copy.accessibleTo = this.accessibleTo;
+        copy.coords = this.coords;
+        return copy;
     }
 
 }
 
 unittest{
-    //TODO add implementation
+    Inventory testInv = new Inventory(1);
+    assert(testInv.add(null));
+    assert(testInv.add(null));
+    //TODO assert(testInv.add(someItem));
+    //TODO assert(!testInv.add(someItem));
 }
--- source/world/HexTile.d
+++ source/world/HexTile.d
@@ -1,24 +1,26 @@
 import app;
 import World;
 import Player;
-import TilePiece;
+import Item;
 import Inventory;
+import std.math;
+import std.conv;
 
 class HexTile{
 
-    public immutable int[] coords;  ///Location of the tile stored as [ringNumber, positionInRing]
-    private double temperature;     ///Part of tile's climate
-    private double humidity;        ///Part of tile's climate (if this is a water tile, determines water salinity)
-    private double soil;            ///Part of tile's climate
-    public bool isWater;            ///Determines if the tile is a water tile
-    private int direction;          ///Direction of wind or water flow
-<<<<<<< HEAD
-    public TilePiece[] improvement; ///Improvement(s) or building(s) or plant(s) that are on this tile
-=======
-    public Inventory contained;     ///Improvement(s) or building(s) or plant(s) that are on this tile
-    private int passabilityCost;    ///How much movement passing this tile costs; isn't the final value, as things in inventory might affect cost
->>>>>>> master
-    public Player owner;            ///The owner of this tile; if none, owner is null
+    public enum Direction{
+        NORTH = 0, NORTH_EAST = 1, SOUTH_EAST = 2, SOUTH = 3, SOUTH_WEST = 4, NORTH_WEST = 5
+    }
+
+    public immutable int[] coords;                  ///Location of the tile stored as [ringNumber, positionInRing]
+    private double temperature;                     ///Part of tile's climate
+    private double water;                           ///Part of tile's climate (if this is a water tile, determines water salinity, otherwise is humidity)
+    private double soil;                            ///Part of tile's climate
+    private double elevation;                       ///Part of the tile's climate
+    public bool isWater;                            ///Determines if the tile is a water tile
+    private Direction direction;                    ///Direction of wind or water flow TODO limit to 0-5
+    public Inventory contained = new Inventory(1);  ///Improvement(s) or building(s) or plant(s) that are on this tile
+    public Player owner;                            ///The owner of this tile; if none, owner is null
 
     /**
      * The constructor for a hextile
@@ -37,8 +39,9 @@ class HexTile{
      *  and then performing different actions based on whether the tile is a) the center, b) straight off the side of the center hex, or
      *  c) just any tile
      * The method will make sure that the adjacent tiles actually exist in the map so that tiles such as map edges don't give adjacent tiles that dont' exist
+     * TODO rework order of returned coordinates such that order is: NORTH, NORTHEAST, SOUTHEAST, SOUTH, SOUTHWEST, NORTHWEST (clockwise starting north)
      */
-    public int[][] getAdjacentTiles(){
+    public int[][] getAdjacentCoords(){
         int[][] adjacentCandidates = null;
         int cornerNum = (this.coords[0] == 0)? 0 : this.coords[1] / this.coords[0];
         if(coords[0] == 0){
@@ -67,11 +70,38 @@ class HexTile{
         return adjacentTiles;
     }
 
+    /*
+     * Returns a tile of distance 1 away from this tile in the direction given
+     * TODO this function won't work because getAdjacentCoords returns adjacencies in an arbitrary order
+     * Params:
+     *      directionOfAdjacent = the direction enum or integer of which side the given adjacent coordinate should be
+     */
+    public int[] getAdjacentCoordInDirection(Direction directionOfAdjacent){
+        return this.getAdjacentCoords()[directionOfAdjacent];
+    }
+
     /**
      * Gets how much movement the tile costs based on the tiles movement cost and its items' movement cost
      */
-    public int getPassabilityCost(){
-        return this.passabilityCost + this.contained.getCollectiveMovementCost();
+    public double getPassabilityCost(){
+        return 1 + to!int(ceil(this.contained.getCollectiveMovementCost()));
+    }
+
+    /**
+     * Makes a copy of this hextile with the same instancedata as this hextile
+     * If this hextile were to change, the copy wouldn't reflect those changes
+     * If the owner of the copy changes, that change is reflected in the copy
+     */
+    public HexTile clone(){
+        HexTile copy = new HexTile(this.coords);
+        copy.temperature = this.temperature;
+        copy.water = this.water;
+        copy.soil = this.soil;
+        copy.isWater = this.isWater;
+        copy.direction = this.direction;
+        copy.contained = this.contained.clone();
+        copy.owner = this.owner;
+        return copy;
     }
 
 }
@@ -82,11 +112,11 @@ unittest{
     import std.random;
     int ringNumsToTest = 5;
     mainWorld = new World(ringNumsToTest);
-    writeln("Adjacencies of (0, 0) : ", mainWorld.getTileAt([0, 0]).getAdjacentTiles());
+    writeln("Adjacencies of (0, 0) : ", mainWorld.getTileAt([0, 0]).getAdjacentCoords());
     int testRunNum = 4;
     for(int i = 0; i < testRunNum; i++){
         int ringNum = uniform(0, ringNumsToTest);
         int pos = uniform(0, getSizeOfRing(ringNum));
-        writeln("Adjacencies of (", ringNum, ", ", pos, ") : ", mainWorld.getTileAt([ringNum, pos]).getAdjacentTiles());
+        writeln("Adjacencies of (", ringNum, ", ", pos, ") : ", mainWorld.getTileAt([ringNum, pos]).getAdjacentCoords());
     }
 }
