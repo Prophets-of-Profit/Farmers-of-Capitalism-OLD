093aaaae712011f3a655bb977ef7a10914c57983
igor-pea <epgorokhovsky01@bvsd.org>
2017-07-30 14:44:41 -0600

    Added abstract class Event and main object.

--- source/app.d
+++ source/app.d
@@ -1,10 +1,32 @@
 module app;
 
 import world.World;
+import player.Player;
 
-static World mainWorld; ///The main world of the entire game
+static Main main; ///The entire game
 
-//TODO add main class that can be serialized and deserialized and is saved as a serialized object with all the data inside of it
+/**
+ * The entire game class
+ */
+class Main{
+
+    public World mainWorld;         ///The world on which the game is played.
+    public Player[] players;        ///The players of the game. Length of array is how many players.
+
+    /*
+     * "You've beat me at my own game!"
+     * "Don't fool yourself; you were never even a Player(int[] coords);
+     */
+
+    this(int numPlayers, int worldSize){
+        for(int i = 0; i < numPlayers; i++){
+            players ~= new Player([0,0]);       //TODO: Add corner selection for player placement.
+        }
+        mainWorld = new World(worldSize);
+    }
+
+}
+
 void main(){
 
 }
