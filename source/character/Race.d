37823cacd9131949e8986a22fc745738d7f96ac7
Elia Gorokhovsky <epgorokhovsky01@bvsd.org>
2017-09-02 13:42:26 -0600

    Removed Pantheon

--- source/character/Race.d
+++ source/character/Race.d
@@ -1,13 +1,8 @@
 module character.Race;
 
-<<<<<<< HEAD
 /**
  * Guides behavior, appearance, and more
  */
 immutable class Race{
 
-=======
-class Race{
-    
->>>>>>> 9e21a1ef5e49a86f6aeab79f44fd809395033812
 }
