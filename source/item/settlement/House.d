2d1a67674696116abc06ff272dbc4e086a97efe0
Elia Gorokhovsky <epgorokhovsky01@bvsd.org>
2017-09-02 14:14:00 -0600

    Fixed Kadin's errors in Settlement-Dev

--- source/item/settlement/House.d
+++ source/item/settlement/House.d
@@ -1,12 +1,15 @@
 module item.settlement.House;
 
-import std.algoritm;
+import std.algorithm;
 import std.array;
+import std.conv;
 import std.random;
 
-import item.settlement.Settlement;
-import item.Item;
+import character.Character;
 import item.Inventory;
+import item.Item;
+import item.settlement.Settlement;
+import world.World;
 
 class House : Item{
 
@@ -14,86 +17,95 @@ class House : Item{
     int livingSpace;                ///The amount of characters that can live in this house
     int landUsed;                   ///The amount of land this house takes up
 
-    /*
+    /**
      * Constructs a new House object
      *  Params:
      *      settlement = the settlement this house is being built in
      *      space = the number of characters that can live in this house
      *      land = the amount of land that this house takes up
-     **/
+     */
     this(Settlement settlement, int space, int land){
         this.settlement = settlement;
         this.livingSpace = space;
         this.landUsed = land;
     }
 
-    /*
+    /**
      * Returns the leader of the settlement this house is in
-     **/
-    @Override Character getOwner(){
+     */
+    override Character getOwner(){
         return this.settlement.leader;
     }
 
-    /*
+    /**
      * Returns whether or not the tile at the coordinate is a settlement
-     **/
-    @Override bool canBePlaced(Coordinate placementCandidateCoords){
+     */
+    override bool canBePlaced(Coordinate placementCandidateCoords){
             //TODO: if settlement is on tile, return true
+            return true;
     }
 
-    /*
+    /**
      * Adds zero to the movement cost (houses do not affect movement cost)
-     **/
-    @Override double getMovementCost(Character stepper);
+     */
+    override double getMovementCost(Character stepper){
         return 0;
     }
 
-    /*
+    /**
      * Adds a new house to the settlement, adding extra population space
-     **/
-    @Override bool getPlaced(Character placer, Coordinate newLocation){
+     */
+    override bool getPlaced(Character placer, Coordinate newLocation){
         this.settlement.characters.maxSize += this.livingSpace;
+        return true;
     }
 
-    /*
+    /**
      * Does nothing when stepped on
-     **/
-    @Override void getSteppedOn(Character stepper){}
+     */
+    override void getSteppedOn(Character stepper){}
 
-    /*
+    /**
      * Has no incremental action
-     **/
-    @Override void doIncrementalAction(){}
+     */
+    override void doIncrementalAction(){}
 
-    /*
+    /**
      * A player can choose to stay in the house.
-     **/
-    @Override void doMainAction(Character player){
+     */
+    override void doMainAction(Character player){
         if(this.settlement.characters.countSpaceRemaining > 0){
             this.settlement.characters.add(player);
         }
     }
 
-    /*
+    /**
      * When the house is destroyed, reduce the amount of space available to characters.
-     **/
-    @Override void getDestroyed(Character destroyer){
+     */
+    override void getDestroyedBy(Character destroyer){
         this.getMovedTo(null);
         this.settlement.characters.maxSize -= this.livingSpace;
     }
 
-    /*
+    /**
      * Returns the amount of land this house uses in the city
-     **/
-    @Override int getSize(){
+     */
+    override int getSize(){
         return this.landUsed;
     }
 
-    /*
-     * Returns a copy of this house.
-     **/
-    @Override Item clone(){
+    /**
+     * Returns a copy of this house
+     */
+    override Item clone(){
         return new House(this.settlement, this.livingSpace, this.landUsed);
+    }
+
+    /**
+     * Returns a string representing the house
+     */
+    override string toString(){
+        return "House with "~livingSpace.to!string~" living space and "~landUsed.to!string~" land used.";
     }
 
 }
--- source/item/settlement/Settlement.d
+++ source/item/settlement/Settlement.d
@@ -2,6 +2,7 @@ module item.settlement.Settlement;
 
 import std.algorithm;
 
+import app;
 import character.Character;
 import item.Inventory;
 import item.Item;
@@ -11,14 +12,15 @@ import world.World;
 immutable maxSpaceForTile = 100;
 immutable baseHousingPerTile = 5;
 
-/*
+/**
  * Returns the nearest settlement to the origin
  * Params:
  *      origin = the origin from which to find the nearest settlement
  *      blacklist = a list of settlements that should not be returned by this function
- **/
+ */
 Settlement findNearestSettlement(Coordinate origin, Settlement[] blacklist = []){
     //TODO: implement this function
+    return null;
 }
 
 class Settlement{
@@ -28,87 +30,95 @@ class Settlement{
     Inventory!Item buildings;                ///An inventory of the buildings
     Character leader;                        ///The leader of the settlement
 
-    /*
+    /**
      * Constructs a new settlement.
      * Params:
      *      leader = the character that leads this settlement
      *      location = the location this settlement is at
-     **/
+     */
     this(){
-        workers = new Inventory!Character(baseHouseingPerTile);
+        characters = new Inventory!Character(baseHousingPerTile);
         buildings = new Inventory!Item(maxSpaceForTile);
     }
 
-    /*
+    /**
      * Adds a building to this settlement. Plants cannot be added to settlements.
-     **/
+     */
     void addBuilding(Item building){
         if(building.canBePlaced(this.location)){
             this.buildings.add(building);
         }
     }
 
-    /*
+    /**
      * Returns the settlement's leader
-     **/
-    @Override Character getOwner(){
+     */
+    override Character getOwner(){
         return this.leader;
     }
 
-    /*
+    /**
      * Settlements can be placed on any tile that does not already have a settlement
-     **/
-    @Override bool canBePlaced(Coordinate placementCandidateCoords){
+     */
+    override bool canBePlaced(Coordinate placementCandidateCoords){
         return true;
     }
 
-    /*
+    /**
      * Add a new settlement to the tile's inventory when it is created, assign the leader to be the one that placed it.
-     **/
-    @Override bool getPlaced(Character placer, Coordinate newLocation){
-        mainWorld.getTileAt(newLocation).contained.add(this);
+     */
+    override bool getPlaced(Character placer, Coordinate newLocation){
+        game.mainWorld.getTileAt(newLocation).contained.add(this);
         this.leader = placer;
+        return true;
     }
 
-    /*
+    /**
      * Returns 0; settlements do not impede movement.
-     **/
-    @Override double getMovementCost(Character stepper){
+     */
+    override double getMovementCost(Character stepper){
         return 0;
     }
 
-    @Override void getSteppedOn(Character stepper){}
+    override void getSteppedOn(Character stepper){}
 
-    @Override void doIncrementalAction(){}
+    override void doIncrementalAction(){}
 
-    /*
+    /**
      * A player interacts with the settlement.
-     **/
-    @Override void doMainAction(Character player){
+     */
+    override void doMainAction(Character player){
         //TODO: Make it so the the player can interact with the settlement
     }
 
-    @Override void getDestroyed(Character destroyer){
+    override void getDestroyedBy(Character destroyer){
         //TODO: Make refugees happen when settlement is destroyed
     }
 
-    /*
+    /**
      * Settlements take up all of the space in a tile's inventory; improvements can be added to the settlement's buildings
-     **/
-    @Override int getSize(){
+     */
+    override int getSize(){
         return 1;
     }
 
-    /*
+    /**
      * Returns a copy of this settlement
-     **/
-    @Override Item clone(){
+     */
+    override Item clone(){
         Settlement cloned = new Settlement();
         cloned.leader = this.leader;
         cloned.location = this.location;
         cloned.characters = this.characters.clone();
         cloned.buildings = this.buildings.clone();
         return cloned;
+    }
+
+    /**
+     * Returns a string representing the settlement
+     */
+    override string toString(){
+        return ""; //TODO: Implement toString
     }
 
 }
