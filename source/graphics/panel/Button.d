4a900dce6160c476f1dcbac8c60480a249d28c45
Elia Gorokhovsky <epgorokhovsky01@bvsd.org>
2017-10-01 14:47:01 -0600

    Added basic Panel class and button

---
+++ source/graphics/panel/Button.d
@@ -0,0 +1,14 @@
+module graphics.panel.Button;
+
+import derelict.sdl2.sdl;
+import derelict.sdl2.image;
+
+class Button : Panel{
+
+    SDL_Surface* hoverImage;
+    SDL_Surface* pressImage;
+
+    override void displayPanel(){
+        //TODO: Implement
+    }
+}
