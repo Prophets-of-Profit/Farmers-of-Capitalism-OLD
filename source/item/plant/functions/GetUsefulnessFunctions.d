f50dde0007f42b85faf0eef0f996901f28def438
Elia Gorokhovsky <epgorokhovsky01@bvsd.org>
2017-10-20 12:54:07 -0600

    Added functions to determine plant usefulness via traits

--- source/item/plant/functions/GetUsefulnessFunctions.d
+++ source/item/plant/functions/GetUsefulnessFunctions.d
@@ -4,9 +4,23 @@ import character.Character;
 import item.plant.Plant;
 
 /**
- * Determines plant usefulness based on how difficult all of its traits are to get
+ * Determines plant usefulness based on how rare its expressed traits are
  */
-double distanceFromAllTraitsFromOrigin(Plant forWhom){
-    //TODO
-    return 0;
+double phenotypeQuality(Plant forWhom){
+    totalDistance = 0
+    foreach(i; forWhom.usableTraits){
+        totalDistance += distance(forWhom.usableTraits[i].difficulty)
+    }
+    return totalDistance;
+}
+
+/**
+ * Determines plant usefulness based on how rare its carried genes are
+ */
+double genotypeQuality(Plant forWhom){
+    totalDistance = 0
+    foreach(i; forWhom.traits){
+        totalDistance += distance(forWhom.traits[i].difficulty)
+    }
+    return totalDistance;
 }
