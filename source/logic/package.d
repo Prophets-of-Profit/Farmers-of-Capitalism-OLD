a6c22edc43346596897c999607c55dfba3a1a44a
Elia Gorokhovsky <epgorokhovsky01@bvsd.org>
2017-12-30 16:30:15 -0700

    Added Range and Climate structs

--- source/logic/package.d
+++ source/logic/package.d
@@ -4,4 +4,5 @@ public import logic.event;
 public import logic.item;
 public import logic.Market;
 public import logic.Player;
+public import logic.Range;
 public import logic.world;
--- source/logic/world/Hex.d
+++ source/logic/world/Hex.d
@@ -1,5 +1,7 @@
 module logic.world.Hex;
 
+import logic.Range;
+import logic.world.Climate;
 import logic.world.Coordinate;
 
 /**
@@ -8,14 +10,25 @@ import logic.world.Coordinate;
  */
 class Hex { 
 
+    Climate climate; ///Stores the tile's climate
     immutable Coordinate location; ///Stores the location of the hex
+    Range!(0, 1) elevation; ///Stores the elevation of the hex as a percentage of the highest elevation in the world
+
+    /**
+     * Gets the linear distance to the nearest water tile
+     * TODO
+     */
+    @property int proximityToWater() {
+        return 0;
+    }
 
     /**
      * The Hex constructor
-     * Creates a hex based on its location
+     * Creates a hex based on its location and climate
      */
-    this(Coordinate location) {
+    this(Coordinate location, Climate climate) {
         this.location = location;
+        this.climate = climate;
     }
 
 }
