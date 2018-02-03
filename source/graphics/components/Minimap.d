65f759dd2f7a5334fd2e3f0495afb9000a5226b1
Elia Gorokhovsky <epgorokhovsky01@bvsd.org>
2018-02-03 16:36:32 -0700

    Added elementary zooming function

--- source/graphics/components/Minimap.d
+++ source/graphics/components/Minimap.d
@@ -15,6 +15,7 @@ class Minimap(uint worldSize) : Component {
     GameWorld!worldSize world; ///The world this minimap should represent
     iRectangle _location; ///Where the component is
     int sideLength = 20; ///The length of the hex sides; used in zooming
+    int scrollValue; ///The total mouse wheel displacement
 
     /**
      * Sets the minimap's location
@@ -37,12 +38,20 @@ class Minimap(uint worldSize) : Component {
         super(container);
         this._location = location;
         this.world = world;
+        scrollValue = this.container.mouse.totalWheelDisplacement.y;
     }
 
     /**
-     * Minimaps don't do anything with events
+     * Handles zoom function
+     * TODO: fix reference point
      */
     void handleEvent(SDL_Event event) {
+        if(this.location.contains(this.container.mouse.location)) {
+            //Equals is the plus key
+            immutable scrollValue = this.scrollValue;
+            this.scrollValue = this.container.mouse.totalWheelDisplacement.y;
+            this.sideLength += this.scrollValue - scrollValue;
+        }   
     }
 
     /**
@@ -57,7 +66,7 @@ class Minimap(uint worldSize) : Component {
             this.container.renderer.fillPolygon!6(new iPolygon!6(getCenterHexagonVertices(                
                 new iVector(cast(int) (center.x - this.sideLength / 2 + coord.q * hexBase.x * this.sideLength + coord.r * hexBase.x * this.sideLength / 2),
                 cast(int) (center.y - this.sideLength / 2 + coord.r * -1.5 * this.sideLength)),
-                20
+                this.sideLength
             )), Color(cast(ubyte) ((abs(coord.q) * 255 / 2 + 100) % 255), cast(ubyte) ((abs(coord.r) * 255 / 2 + 100) % 255), cast(ubyte) ((abs(coord.s) * 255 / 2 + 100) % 255)));
         }
     }
