d73a4dd6364ec087f7de8021735daaca62d03245
Saurabh Totey <SaurabhTotey@gmail.com>
2018-03-02 12:19:03 -0700

    Moved certain Item fields into Inventory when applicable

--- source/logic/item/Inventory.d
+++ source/logic/item/Inventory.d
@@ -1,6 +1,7 @@
 module logic.item.Inventory;
 
 import logic.item.Item;
+import logic.world.Coordinate;
 
 /**
  * A class that handles storing and transferring items
@@ -8,11 +9,15 @@ import logic.item.Item;
 class Inventory {
 
     Item[] items; ///The items of the inventory
+    Coordinate location; ///Where the inventory is situated
+    immutable bool isPlaced; ///Whether the inventory is placed or not (eg. any items in this inventory are counted as down on the board)
 
     /**
      * Creates an inventory from a list of items
      */
-    this(Item[] contained...) {
+    this(bool isPlaced, Coordinate location, Item[] contained...) {
+        this.isPlaced = isPlaced;
+        this.location = location;
         this.items = contained;
     }
 
--- source/logic/item/Item.d
+++ source/logic/item/Item.d
@@ -1,23 +1,32 @@
 module logic.item.Item;
 
 import graphics.Constants;
-import logic.world.Coordinate;
+import logic.item.Inventory;
+import logic.Player;
 
 /**
  * The Platonic ideal of an item
  * Outlines what an item should be and do
  */
-interface Item {
+abstract class Item {
 
-    @property Coordinate location(); ///Where the item is
-    @property Image representation(); ///How the item looks
-    @property bool isPlaced(); ///Whether the item is placed or not
+    Inventory container; ///Where the item is currently located
+
+    @property Image representation(); ///How the item looks on the GUI
     @property double completion(); ///How close this item is to being "completed"; what completion entails is up to the item
-    @property int movementCostChange(); ///What additive change in movement cost this item has
+    @property int movementCostChange(); ///What additive change in movement cost this item has when placed
 
-    void onStep(); ///What happens when the item is stepped on TODO: take character in as a parameter
+    /**
+     * All items need to have an inventory
+     */
+    this(Inventory container) {
+        this.container = container;
+    }
+
+    void onStep(Player actor); ///What happens when the item is stepped on
     void incrementalAction(); ///What this item does every turn
-    void mainAction(); ///What this item does when interacted with TODO: take character in as a parameter
-    void onDestroy(); ///What happens when a player destroys this item
+    void mainAction(Player actor); ///What this item does when interacted with
+    void onCreate(Player actor); ///What this item does when a player creates it
+    void onDestroy(Player actor); ///What happens when a player destroys this item
 
 }
