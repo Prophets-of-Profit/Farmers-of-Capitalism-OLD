5bd2df9077293211b31861d607f312e061709b08
Saurabh Totey <SaurabhTotey@gmail.com>
2018-03-11 09:47:19 -0600

    Added InvPanel selection of items

--- source/graphics/components/InvPanel.d
+++ source/graphics/components/InvPanel.d
@@ -8,25 +8,12 @@ import logic.item.Inventory;
 /**
  * A component which displays an inventory to the screen
  */
-class InvPanel : Component {
+class InvPanel : Button {
 
-    private iRectangle _location; ///The location and bounds of the inventory panel
     Inventory inventory; ///The inventory this component displays
     immutable Color bgColor = Color(100, 100, 100); ///The background color of the inventory panel
-
-    /**
-     * Returns the location
-     */
-    override @property iRectangle location() {
-        return this._location;
-    }
-
-    /**
-     * Sets the location
-     */
-    override @property void location(iRectangle newLocation) {
-        this._location = newLocation;
-    }
+    iVector selectedItem; ///The location of the user's last selected item
+    immutable Color selectedItemColor = Color(150, 150, 255, 100);
 
     /**
      * Gets how much space goes between each box
@@ -40,7 +27,7 @@ class InvPanel : Component {
      * Is based off of the dimensions of this panel
      */
     @property int itemDimension() {
-        return this._location.w / this.columns; 
+        return this.location.w / this.columns; 
     }
 
     /**
@@ -70,17 +57,49 @@ class InvPanel : Component {
      * Contructs a new inventory panel contained in the given display
      */
     this(Display container, iRectangle location, Inventory inventory) {
-        super(container);
+        super(container, location);
         this.inventory = inventory;
-        this._location = location;
+        this.selectedItem = new iVector(-1);
     }
 
     /**
      * Handles inventory slider (if applicable) and clicking on items in the inventory as well as hovering over items
-     * TODO:
      */
-    void handleEvent(SDL_Event event) {
+    override void handleEvent(SDL_Event event) {
+        super.handleEvent(event);
+        if (!this.isHovered) {
+            return;
+        }
+        //TODO: mouse hover
+        //TODO: scroll bar
+    }
 
+    /**
+     * When the invpanel, is clicked, this looks for where it was clicked and sets the selected item
+     */
+    override void action() {
+        foreach (r; 0..this.rows) {
+            foreach (c; 0..this.columns) {
+                if (this.locationOf(r, c).contains(this.container.mouse.location)) {
+                    this.selectedItem.x = c;
+                    this.selectedItem.y = r;
+                    return;
+                }
+            }
+        }
+        this.selectedItem = -1;
+    }
+
+    /**
+     * Returns the location of a box in the given colulmn and row
+     */
+    iRectangle locationOf(int row, int column) {
+        return new iRectangle(
+            this.location.x + column * this.itemDimension + this.padding / 2,
+            this.location.y + row * this.itemDimension + this.padding / 2,
+            this.itemDimension - this.padding,
+            this.itemDimension - this.padding
+        );
     }
 
     /**
@@ -92,17 +111,15 @@ class InvPanel : Component {
         foreach(i; iota(0, this.inventory.maxItems < 0? this.inventory.length + 1 : this.inventory.maxItems)) {
             immutable currentColumn = cast(int) i % this.columns;
             immutable currentRow = cast(int) i / this.columns;
-            iRectangle bgBox = new iRectangle(
-                this.location.x + currentColumn * this.itemDimension + this.padding / 2,
-                this.location.y + currentRow * this.itemDimension + this.padding / 2,
-                this.itemDimension - this.padding,
-                this.itemDimension - this.padding
-            );
-            this.container.renderer.fillRect(bgBox, PredefinedColor.WHITE);
+            iRectangle box = this.locationOf(currentRow, currentColumn);
+            this.container.renderer.fillRect(box, PredefinedColor.WHITE);
             if (i < this.inventory.length) {
-                this.container.renderer.copy(textures[this.inventory[i].representation], bgBox);
+                this.container.renderer.copy(textures[this.inventory[i].representation], box);
             }
-            this.container.renderer.drawRect(bgBox, PredefinedColor.BLACK);
+            if (currentColumn == this.selectedItem.x && currentRow == this.selectedItem.y) {
+                this.container.renderer.fillRect(box, this.selectedItemColor);
+            }
+            this.container.renderer.drawRect(box, PredefinedColor.BLACK);
         }
     }
 
